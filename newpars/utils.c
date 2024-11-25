55bc988ab09c6f11768e84d58890faabcadabb68
Achraf Meziouni <amezioun@c2r11p2.1337.ma>
2024-11-25 16:05:40 +0100

    checking Null

--- newpars/utils.c
+++ newpars/utils.c
@@ -6,7 +6,7 @@
 /*   By: amezioun <amezioun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 10:09:38 by amezioun          #+#    #+#             */
-/*   Updated: 2024/11/13 10:09:39 by amezioun         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:44:21 by amezioun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int	ft_strcmp(char *str, char *target)
 {
+	if(!str && !target)
+		return 0;
+	if(!str)
+		return -1;
+	if(!target)
+		return 1;
 	while (*str && (*str == *target))
 	{
 		str++;
